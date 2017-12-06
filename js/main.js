c562f970132e13ffe27b2e79ab8fd1be2b4805d7
jmacias <jmacias@b7-04.local>
2017-12-05 19:31:18 -0600

    starting internal pages

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var logoMenu = document.getElementById('logoMenu');
 function toggleMenu() {
 	navigation.classList.toggle('toggleMenu');
 	logoMenu.classList.toggle('hiddenMainLogo');
+	iconOpenMenu.classList.toggle('menu-bg--exit');
 }
 
 iconOpenMenu.addEventListener('click', toggleMenu);
